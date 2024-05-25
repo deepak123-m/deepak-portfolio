15534361a54bd775b5c52179f7c5b43011203d48
deepak123-m <deepak79935@gmail.com>
2024-05-26 00:48:33 +0530

    changed

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import arrayDestruct from "../assets/portfolio/note.png";
-import installNode from "../assets/portfolio/installNode.jpg";
 import navbar from "../assets/portfolio/navbar.jpg";
 import reactParallax from "../assets/portfolio/reactParallax.jpg";
 import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
